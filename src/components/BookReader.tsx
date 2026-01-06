c109bd129e14e6dc79ac680141755590e9a50384
piotr-e8 <piotr@entereighth.com>
2026-01-06 09:59:55 +0100

    Update BookReader.tsx

--- src/components/BookReader.tsx
+++ src/components/BookReader.tsx
@@ -51,7 +51,7 @@ export function BookReader({ event }: BookReaderProps) {
   const bookSlug =
   event.tags.find((t) => t[0] === 'book')?.[1] ??
   event.tags.find((t) => t[0] === 'd')?.[1];
-const bookRoot = useBookRoot(bookSlug);
+const bookRoot = useBookRoot(bookSlug, event.pubkey);
 const toc = extractTOC(bookRoot.data?.content);
 const [tocOpen, setTocOpen] = useState(false);
 const params = useParams();
